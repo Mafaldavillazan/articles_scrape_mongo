fe7d81276859e338eacc26091480abb29be0894f
Mafalda <mafalda.villazan@gmail.com>
2019-08-10 13:47:13 -0700

    Improving the ui of the comment secriton

--- public/app.js
+++ public/app.js
@@ -1,38 +1,12 @@
 // Grab the articles as a json
 $.getJSON("/articles", function (data) {
-  for (var i = 0; i < 5; i++) {
-    // Display the values with handle bars
-    $("#articles").append("<div class='card' style='width: 35rem;' id='article' data-id='" + data[i]._id + "'>"
-      + " <div class='card-body'>"
-      + "<h5 class='card-title'>"
-      + data[i].title
-      + "<h5/>"
-      + "<p class='card-text'>"
-      + data[i].summary
-      + "</p>"
-      + "<a class='btn btn-info' href="
-      + data[i].link
-      + "> Link to article </a>"
-      + "</div>"
-      + "</div>");
-  }
 
-  for (var i = 5; i < 10; i++) {
-    // Display the values with handle bars
-    $("#articles2").append("<div class='card' style='width: 35rem;' id='article' data-id='" + data[i]._id + "'>"
-      + " <div class='card-body'>"
-      + "<h5 class='card-title'>"
-      + data[i].title
-      + "<h5/>"
-      + "<p class='card-text'>"
-      + data[i].summary
-      + "</p>"
-      + "<a class='btn btn-info' href="
-      + data[i].link
-      + "> Link to article </a>"
-      + "</div>"
-      + "</div>");
-  }
+  article1 = "#articles"
+  getArticles(article1, 5, data)
+
+  article2 = "#articles2"
+  getArticles(article2, 10, data)
+
 });
 
 // Creating the note
@@ -41,6 +15,7 @@ $(document).on("click", "#article", function () {
   $("#notes").empty();
   var thisId = $(this).attr("data-id");
 
+
   //Get the article id
   $.ajax({
     method: "GET",
@@ -48,13 +23,8 @@ $(document).on("click", "#article", function () {
   })
     .then(function (data) {
 
-
-      //Create the note to display
-      $("#notes").append("<h4>" + data.title + "</h4>");
-      $("#notes").append("<textarea id='titleinput' name='title' >");
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      $("#notes").append("<button data-id='" + data._id + "' id='savecomment'>Save Note</button>");
-
+      commentDisplay(data)
+      
       // If there's a note in the article
       if (data.comment) {
         renderComment(data)
@@ -74,9 +44,10 @@ $(document).on("click", "#article", function () {
 
       // Posting the note to that particular article
       $(document).on("click", "#savecomment", function () {
-
+        
+        
         var thisId = $(this).attr("data-id");
-
+      
         $.ajax({
           method: "POST",
           url: "/articles/" + thisId,
@@ -89,6 +60,7 @@ $(document).on("click", "#article", function () {
             console.log(data);
             $("#notes").empty()
             $("#comment").empty()
+            c
           })
 
         $("#titleinput").val("");
@@ -101,6 +73,7 @@ $(document).on("click", "#article", function () {
 //+++++++
 // Functions
 
+//Render all comments
 function renderComment(data) {
 
   for (var i = 0; i < data.comment.length; i++) {
@@ -117,4 +90,40 @@ function renderComment(data) {
     $("#comment").append($newDiv);
   }
 
+}
+
+//Display the articles
+function getArticles(jqueyCall, num, data) {
+  for (var i = 0; i < num; i++) {
+    // Display the values with handle bars
+    $(jqueyCall).append("<div class='card' style='width: 30rem;' id='article' data-id='" + data[i]._id + "'>"
+      + " <div class='card-body'>"
+      + "<h5 class='card-title'>"
+      + data[i].title
+      + "<h5/>"
+      + "<p class='card-text'>"
+      + data[i].summary
+      + "</p>"
+      + "<a class='btn btn-info' href="
+      + data[i].link
+      + "> Link to article </a>"
+      + "</div>"
+      + "</div>");
+  }
+}
+
+//Create the note to display
+function commentDisplay(data){
+  
+  $("#notes").append("<h5 class='card-title'>" + data.title + "</h5>");
+  $("#notes").append("<p class='form-inline card-text'>What do you want to say?</p>")
+
+  
+  var $newDiv = $("<div class= col-3>")
+  $($newDiv).append("<input type='text' class='form-control center' id='titleinput' placeholder='title:' name='title'>");
+  $($newDiv).append("<textarea type='text' class='form-control center'  id='bodyinput'  placeholder='body:' name='body'></textarea>");
+  $("#notes").append($newDiv)
+
+
+  $("#notes").append("<button class='btn btn-primary' data-id='" + data._id + "' id='savecomment'>Save Note</button>");
 }
