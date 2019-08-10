6b1f340913529a00609710c04414de65c08fce70
Mafalda <mafalda.villazan@gmail.com>
2019-08-10 13:57:58 -0700

    Changing the way the comments are displayed

--- public/app.js
+++ public/app.js
@@ -2,10 +2,10 @@
 $.getJSON("/articles", function (data) {
 
   article1 = "#articles"
-  getArticles(article1, 5, data)
+  getArticles(article1, 0, 5, data)
 
   article2 = "#articles2"
-  getArticles(article2, 10, data)
+  getArticles(article2, 5, 10, data)
 
 });
 
@@ -24,7 +24,7 @@ $(document).on("click", "#article", function () {
     .then(function (data) {
 
       commentDisplay(data)
-      
+
       // If there's a note in the article
       if (data.comment) {
         renderComment(data)
@@ -44,10 +44,10 @@ $(document).on("click", "#article", function () {
 
       // Posting the note to that particular article
       $(document).on("click", "#savecomment", function () {
-        
-        
+
+
         var thisId = $(this).attr("data-id");
-      
+
         $.ajax({
           method: "POST",
           url: "/articles/" + thisId,
@@ -75,26 +75,31 @@ $(document).on("click", "#article", function () {
 
 //Render all comments
 function renderComment(data) {
-
   for (var i = 0; i < data.comment.length; i++) {
-    $newDiv = $("<br><div>")
-    $newDiv.append("Title: " + data.comment[i].title);
-    $newDiv.append("<br> About: " + data.comment[i].body)
+    $newDiv = $("<div class='col-sm-3'>")
+    $newCard = $("<div class='card'>")
+    $cardBody = $("<div class='card-body'>")
 
-    $newButton = $("<br> <button class='delete' id='"
+
+    $cardBody.append("<h5 class='card-title'>" + data.comment[i].title + "</h5>");
+    $cardBody.append("<p class='card-text'>" + data.comment[i].body + "</p>")
+
+    $newButton = $("<button class='delete btn btn-info' id='"
       + data.comment[i]._id
-      + "'> X </button>")
+      + "'> delete </button>")
 
-    $newDiv.append($newButton)
+    $cardBody.append($newButton)
 
-    $("#comment").append($newDiv);
+    $newCard.append($cardBody)
+    $newDiv.append($newCard)
+    $("#comment").append($newDiv)
   }
 
 }
 
 //Display the articles
-function getArticles(jqueyCall, num, data) {
-  for (var i = 0; i < num; i++) {
+function getArticles(jqueyCall, num1, num2, data) {
+  for (var i = num1; i < num2; i++) {
     // Display the values with handle bars
     $(jqueyCall).append("<div class='card' style='width: 30rem;' id='article' data-id='" + data[i]._id + "'>"
       + " <div class='card-body'>"
@@ -113,17 +118,17 @@ function getArticles(jqueyCall, num, data) {
 }
 
 //Create the note to display
-function commentDisplay(data){
-  
+function commentDisplay(data) {
+
   $("#notes").append("<h5 class='card-title'>" + data.title + "</h5>");
   $("#notes").append("<p class='form-inline card-text'>What do you want to say?</p>")
 
-  
+
   var $newDiv = $("<div class= col-3>")
   $($newDiv).append("<input type='text' class='form-control center' id='titleinput' placeholder='title:' name='title'>");
   $($newDiv).append("<textarea type='text' class='form-control center'  id='bodyinput'  placeholder='body:' name='body'></textarea>");
   $("#notes").append($newDiv)
 
 
-  $("#notes").append("<button class='btn btn-primary' data-id='" + data._id + "' id='savecomment'>Save Note</button>");
+  $("#notes").append("<button class='btn btn-info' data-id='" + data._id + "' id='savecomment'>Save Note</button>");
 }
